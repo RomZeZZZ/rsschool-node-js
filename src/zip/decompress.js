a35c14e7c6c986f24acac30f9cb386d230d36cde
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2023-06-27 00:42:52 +0300

    feat: add decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,30 +1,41 @@
 import { createReadStream, createWriteStream } from 'fs';
-import { resolve, join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-import zlib from 'zlib';
+import { resolve } from 'path';
+import { basename, extname } from 'node:path';
+import { createBrotliDecompress } from 'zlib';
 import { access, constants, stat } from 'fs/promises';
+import { pipeline as pipelineAsync } from 'stream/promises';
 const decompress = async (pathToFile, pathToDest) => {
-  try {
-    const rootPath = dirname(fileURLToPath(import.meta.url));
-    const compressedFilePath = resolve(join(rootPath, 'files', 'archive.gz'));
-    const compressFileExist = await access(compressedFilePath, constants.F_OK)
-    .then(() => { return true })
-    .catch(() => { return false });
-    if (compressFileExist) {
-      const decompressedFilePath = resolve(join(rootPath, 'files', 'fileToCompress.txt'));
-      const readStream = createReadStream(compressedFilePath);
-      const writeStream = createWriteStream(decompressedFilePath);
-      const gunzip = zlib.createGunzip();
-      readStream.pipe(gunzip).pipe(writeStream);
-      writeStream.on('finish', () => {
-        console.log('File decompression completed.');
-      });
-    } else {
-      throw new Error('Compress file missing.');
+    try {
+        const initialPath = resolve(process.cwd(), pathToFile);
+        const pathToDir = resolve(process.cwd(), pathToDest);
+        let decompressedFilePath;
+        const stats = await stat(pathToDir)
+        .then((data) => { return data.isDirectory() })
+        .catch(() => { return false });
+        const isfileExist = await access(initialPath, constants.F_OK)
+        .then(() => { return true })
+        .catch(() => { return false });
+        if (stats && isfileExist) {
+            const isDirectoryExist = await access(pathToDir, constants.F_OK)
+            .then(() => { return true })
+            .catch(() => { return false });
+            if (isDirectoryExist) {
+                decompressedFilePath = resolve(pathToDir, basename(initialPath, extname(initialPath)) + '.txt');
+            }
+        } else {
+            decompressedFilePath = pathToDir;
+        }
+        if (isfileExist) {
+            const readStream = createReadStream(initialPath);
+            const writeStream = createWriteStream(decompressedFilePath);
+            const decompressStream = createBrotliDecompress();
+            await pipelineAsync(readStream, decompressStream, writeStream);
+        } else {
+            throw new Error('Compress file missing or wrong dest path');
+        }
+    } catch (error) {
+        console.log(error.message)
     }
-  } catch (error) {
-    console.log(error.message)
-  }
 };
 
 export default decompress;
